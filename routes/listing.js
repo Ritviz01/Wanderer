508102c76aa0461c94d77c92c60fd2b9b89f737c
Ritviz <ritviztiwari07@gmail.com>
2025-08-02 00:01:24 +0530

    Restyling the review section and added cloud as the image storage used cloudinary for that

--- routes/listing.js
+++ routes/listing.js
@@ -1,100 +1,46 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-const wrapAsync = require("../utils/wrapAsync.js")
-const Listing = require("../models/listing.js")
-const { isLoggedIn, isOwner, validateListing } = require("../middleware.js")
+const wrapAsync = require("../utils/wrapAsync.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
+const listingController = require("../controllers/listings.js");
+const multer = require('multer')
+const { storage } = require("../cloudConfig.js")
+const upload = multer({ storage })
 
-
-//Index Route
-
-router.get('/', wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({})
-    res.render("listings/index.ejs", { allListings })
-
-}))
-
-//create route
-router.get('/new', isLoggedIn, (req, res) => {
-
-    res.render('listings/new.ejs')
-
-})
+router.route("/")
+    .get(wrapAsync(listingController.index))
+    .post(
+        isLoggedIn,
+        upload.single('listing[image]'),
+        validateListing,
+        wrapAsync(listingController.createListing)
+    );
 
 
 
+// New Route
+router.get('/new', isLoggedIn, listingController.renderNewForm);
 
-//show route 
-router.get("/:id", wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id)
-        .populate({
-            path: "reviews",
-            populate: {
-                path: "author",
-            },
-        })
-        .populate("owner")
-    if (!listing) {
-        req.flash("error", "Listing You Requested For Does Not Exists")
-        res.redirect("/listings")
-    }
-    res.render("listings/show.ejs", { listing })
-}))
+router.route("/:id")
+    .get(wrapAsync(listingController.showListing))
+    .put(
+        isLoggedIn,
+        isOwner,
+        upload.single('listing[image]'),
+        validateListing,
+        wrapAsync(listingController.updateListing)
+    )
+    .delete(
+        isLoggedIn,
+        isOwner,
+        wrapAsync(listingController.deleteListing)
+    );
 
-
-
-
-//create route = Read 
-router.post("/", isLoggedIn, validateListing, wrapAsync(async (req, res, next) => {
-
-    const newListing = new Listing(req.body.listing)
-    newListing.owner = req.user._id;
-    await newListing.save();
-    req.flash("success", "New Listing Created")
-    res.redirect('/listings')
-
-
-
-}))
-
-//edit route
+// Edit Route
 router.get("/:id/edit",
     isLoggedIn,
     isOwner,
-    wrapAsync(async (req, res) => {
-        let { id } = req.params;
-        const listing = await Listing.findById(id)
-        if (!listing) {
-            req.flash("error", "Listing You Requested For Does Not Exists")
-            res.redirect("/listings")
-        }
-        res.render("listings/edit.ejs", { listing })
-    }))
-
-//update route
-router.put("/:id",
-    isLoggedIn,
-    isOwner,
-    validateListing,
-    wrapAsync(async (req, res) => {
-
-        if (!req.body.listing) {
-            throw new ExpressError(400, "Enter the valid data for listing")
-        }
-        await Listing.findByIdAndUpdate(id, { ...req.body.listing })
-        req.flash("success", "Listing Updated Created")
-        res.redirect(`/listings/${id}`);
-    }))
-
-//delete route
-router.delete("/:id", isLoggedIn, isOwner, wrapAsync(async (req, res) => {
-
-    let { id } = req.params;
-    let deleteListing = await Listing.findByIdAndDelete(id)
-    console.log(deleteListing);
-    req.flash("success", "Listing Deleted")
-    res.redirect('/listings')
-
-}))
+    wrapAsync(listingController.editListing)
+);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -5,42 +5,19 @@ const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport")
 const { saveRedirectUrl } = require("../middleware.js")
 
-router.get('/signup', (req, res) => {
-    res.render('users/signup.ejs');
-})
+const userController = require("../controllers/user.js")
 
-router.post("/signup", wrapAsync(async (req, res) => {
-    const { username, email, password } = req.body;
-    const newUser = new User({ email, username });
-    const registeredUser = await User.register(newUser, password);
-    console.log(registeredUser);
-    req.login(registeredUser, (err) => {
-        if (err) {
-            return next(err);
-        }
-        req.flash('success', 'Welcome to Wanderer')
-        return res.redirect('/listings');
-    })
+router.route("/signup")
+    .get(userController.renderSignupForm)
+    .post(wrapAsync(userController.signup));
 
-}));
 
-router.get("/login", (req, res) => {
-    res.render('users/login.ejs');
-})
 
-router.post("/login", saveRedirectUrl, passport.authenticate('local', { failureRedirect: "/login", failureFlash: true }), async (req, res) => {  //Passport provides an authenticate() function, which is used as route middleware to authenticate requests.
-    req.flash("success", "Welcome back to Wanderer  ")
-    res.redirect(res.locals.redirectUrl || "/listings")
-})
+router.route("/login")
+    .get(userController.renderLoginForm)
+    .post(saveRedirectUrl, passport.authenticate('local', { failureRedirect: "/login", failureFlash: true }), userController.login)
 
-router.get("/logout", (req, res) => {
-    req.logout((err) => {
-        if (err) {
-            return next(err);
 
-        }
-        req.flash("success", "you are logged out!");
-        res.redirect("/listings");
-    })
-})
+
+router.get("/logout", userController.logout)
 module.exports = router;
